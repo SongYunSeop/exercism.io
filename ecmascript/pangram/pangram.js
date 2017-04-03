1758df8cbb3d88c17150e2f524eff1d17ef549dc
SongYunSeop <dbstjq91@gmail.com>
2017-04-03 17:27:40 +0900

    [ECMAScript] Refacoring Pangram

--- ecmascript/pangram/pangram.js
+++ ecmascript/pangram/pangram.js
@@ -1,24 +1,11 @@
 class Pangram {
 	constructor(sentence) {
-		// convert sentence lower case & remove whitespace
-		this.sentence = sentence.toLowerCase().replace(/\s/g,'');
+		// convert sentence lower case & remove non alphabet
+		this.sentence = sentence.toLowerCase().replace(/[^a-z]/g,'');
 	}
 
 	isPangram() {
-		if (this.sentence.length < 26) return false;
-
-		const checkArr = new Array(26).fill(0);
-
-		const aCode = 'a'.charCodeAt()
-		const zCode = 'z'.charCodeAt()
-
-		for(let s of this.sentence) {
-			const code = s.charCodeAt()
-			if (code < aCode || code > zCode) continue;
-			checkArr[s.charCodeAt() - aCode] = 1
-		}
-
-		return checkArr.filter((e) => { return e == 1 }).length == 26
+		return new Set(this.sentence.split('')).size == 26
 	}
 }
 
